05cf281e3d51a92a90c9e288339cab14846243c3
cjb873 <cjb873@nau.edu>
2025-04-05 12:10:33 -0700

    Only collect data, don't animate

--- src/callback.py
+++ src/callback.py
@@ -1,5 +1,4 @@
 from neuromancer.callbacks import Callback
-import matplotlib.pyplot as plt
 
 
 class CallbackViz(Callback):
@@ -7,37 +6,13 @@ class CallbackViz(Callback):
         super().__init__()
         self.figsize = 25
         self.i = 0
+        self.data = None
 
     def end_epoch(self, trainer, output):
-        output = trainer.test(trainer.best_model)
-        nsteps_test = output['test_xn'].shape[-2] - 1
-        nx = output['test_xn'].shape[-1]
-        nu = output['test_u'].shape[-1]
-        nref = output['test_r'].shape[-1]
-        loss = output['test_loss']
-        """
-        xmin = output['test_xmin']
-        xmax = output['test_xmax']
-        umin = output['test_umin']
-        umax = output['test_umax']
+        current_results = trainer.test(trainer.best_model)
 
-        Umin = umin * np.ones([nsteps_test, nu])
-        Umax = umax * np.ones([nsteps_test, nu])
-        Xmin = xmin * np.ones([nsteps_test+1, nx])
-        Xmax = xmax * np.ones([nsteps_test+1, nx])
-        """
-
-        fig, ax = plt.subplots(2, figsize=(self.figsize, self.figsize))
-        fig.suptitle(f"Test Trajectories at Epoch {self.i}", fontsize=48)
-        ax[0].set_title("State Trajectories", fontsize=24)
-        ax[0].plot(output['test_r'].detach().numpy().reshape(nsteps_test+1,
-                   nref), '--', color="red", label="reference", linewidth=5)
-        ax[0].plot(output['test_xn'].detach().numpy().reshape(nsteps_test+1,
-                   nx), label='policy', linewidth=5)
-        ax[1].set_title("Control Input", fontsize=24)
-        ax[1].plot(output['test_u'].detach().numpy().reshape(nsteps_test, nu),
-                   label='policy', linewidth=5)
-        ax[1].text(-20, -1, f"Loss of Policy: {loss.item():.4f}", fontsize=18)
-        plt.savefig(f"images/{self.i}.png")
-
-        self.i += 1
+        if self.data is None:
+            self.data = {key: [val] for key, val in current_results.items()}
+        else:
+            {key: val.append(current_results[key]) for key, val in
+             self.data.items()}
